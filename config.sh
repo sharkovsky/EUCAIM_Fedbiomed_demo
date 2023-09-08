76eda2b7ff3935fa7be33239369a7f9b24d76437
Francesco Cremonesi <francesco.cremonesi@inria.fr>
2023-09-08 13:53:19 +0200

    Node config files and config script

---
+++ config.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+git clone --depth 1 --branch v4.4.3 git@github.com:fedbiomed/fedbiomed.git
+
+cp etc/* fedbiomed/etc/
+
+
+cd demo_ml
+git clone --depth 1 git@github.com:EUCAIM/demo_ml_data.git
+
+cd ../fedbiomed
+for data_provide in bsc ub forth
+do
+	./scripts/fedbiomed_run node config ${data_provider}.ini configuration create
+	./scripts/fedbiomed_run node config ${data_provider}.ini --add-dataset-from-file ../demo_ml/${data_provider}.json
+done
+
+
+
