4acc9483264cf0ffe48c442a58e1426cac533926
Francesco Cremonesi <francesco.cremonesi@inria.fr>
2023-09-08 14:31:11 +0200

    Update paths

--- config.sh
+++ config.sh
@@ -5,14 +5,16 @@ git clone --depth 1 --branch v4.4.3 git@github.com:fedbiomed/fedbiomed.git
 mkdir fedbiomed/etc
 cp etc/* fedbiomed/etc/
 
-cd demo_ml
-git clone --depth 1 git@github.com:EUCAIM/demo_ml_data.git
+git clone --depth 1 git@github.com:EUCAIM/demo_ml_data.git fedbiomed/envs/vpn/docker/node/run_mounts/data/
+git clone --depth 1 git@github.com:EUCAIM/demo_dl_data.git fedbiomed/envs/vpn/docker/node/run_mounts/data/
+wget -p fedbiomed/envs/vpn/docker/node/run_mounts/data/demo_dl_data https://www.kaggle.com/datasets/paultimothymooney/chest-xray-pneumonia/download?datasetVersionNumber=2
 
-cd ../fedbiomed
+cp demo_ml/*.json fedbiomed/envs/vpn/docker/node/run_mounts/data/demo_ml_data
+
+cd fedbiomed
 for data_provider in bsc ub forth
 do
-	./scripts/fedbiomed_run node config ${data_provider}.ini configuration create
-	./scripts/fedbiomed_run node config ${data_provider}.ini --add-dataset-from-file ../demo_ml/${data_provider}.json
+	./scripts/fedbiomed_run node config ${data_provider}.ini --add-dataset-from-file envs/vpn/docker/node/run_mounts/data/demo_ml_data/${data_provider}.json
 done
 
 
